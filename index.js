5f5d9398e14d365cd8c5aab44aa6e920fc36e52b
shayan <sak704@nyu.edu>
2022-02-20 16:58:55 -0800

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution here
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+const input = "bobby";
+function findMatching(array, string) {
+    const lcNames = string.toLowerCase();
+    //console.log (lcNames);
+    const matDrivers = array.filter(function(names){
+        const drName = names.toLowerCase();
+        return drName === lcNames;
+    });
+    return matDrivers;
+}
+
+console.log(findMatching(drivers,input));
+
+function fuzzyMatch(arr,str) {
+    const lcLet = str.toLowerCase();
+
+    const matDrv = arr.filter(function(name){
+        const drNames = name.toLowerCase();
+        return drNames.slice(0,str.length) === lcLet;
+    });
+
+    return matDrv;
+
+}
+
+function matchName(obj, strg) {
+    const lcName = strg.toLowerCase();
+
+    const matNames = obj.filter(function(nam){
+        const drNam = Object.assign({},nam);
+        //console.log(drNam);
+        return drNam["name"].toLowerCase() === lcName;
+    })
+
+    return matNames;
+}
